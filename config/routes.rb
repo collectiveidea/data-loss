b7802ef65ca878329b35d4b51b8457a721febbff
Daniel Morrison <daniel@collectiveidea.com>
2012-11-08 18:45:24 -0500

    Fix a route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Dataloss::Application.routes.draw do
   match '/auth/:provider/callback', to: 'sessions#create'
   match '/push-the-button' => 'incidents#new', as: 'new_incident'
+  resources :incidents, only: :create
   match ':id' => 'incidents#show', as: 'incident'
   root :to => 'incidents#index'
 end
