891b985772d11b5dff7863dec7966ec035d2606c
Daniel Morrison <daniel@collectiveidea.com>
2012-11-09 09:47:38 -0500

    Added auth failure path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Dataloss::Application.routes.draw do
   match '/auth/:provider/callback', to: 'sessions#create'
+  match '/auth/failure', to: redirect('/')
   match '/push-the-button' => 'incidents#new', as: 'new_incident'
   resources :incidents, only: :create
   match ':id' => 'incidents#show', as: 'incident'
