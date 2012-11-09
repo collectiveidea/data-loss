93781fb2edf11ae873dfd54ecd23d39a8980107f
Daniel Morrison <daniel@collectiveidea.com>
2012-11-09 09:31:30 -0500

    Show the button on your page when signed in.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def user_required
-    redirect_to '/auth/twitter?force_login=true' unless session[:username]
+    redirect_to '/auth/twitter?force_login=true' unless signed_in?
+  end
+
+  def signed_in?
+    session[:username].present?
   end
 end
