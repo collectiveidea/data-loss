ed305071b4b1ac4b6b03cdc63d2456b3282b8deb
Daniel Morrison <daniel@collectiveidea.com>
2012-11-09 09:46:26 -0500

    Expose signed_in? helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  helper_method :signed_in?
 
   def user_required
     redirect_to '/auth/twitter?force_login=true' unless signed_in?
