bc8050b22831e9f0d242059862490f395a6276e1
Daniel Morrison <daniel@collectiveidea.com>
2014-01-20 15:51:17 -0500

    Fix up a broken route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Dataloss::Application.routes.draw do
   match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   match '/auth/failure', to: redirect('/'), via: [:get, :post]
-  get '/push-the-button' => 'incidents#new', as: 'new_incident'
+  match '/push-the-button' => 'incidents#new', as: 'new_incident', via: [:get, :post]
   resources :incidents, only: :create
   get ':id' => 'incidents#show', as: 'incident'
   root :to => 'incidents#index'
