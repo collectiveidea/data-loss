2a195e14e2d4e240a5a9e603ff5c483bcf28ce54
Daniel Morrison <daniel@collectiveidea.com>
2014-01-20 15:29:35 -0500

    Force figaro to load before using it

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,8 @@ module Dataloss
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = ENV["LOCALE"]
+    config.i18n.enforce_available_locales = true
+    Figaro.load
+    config.i18n.default_locale = Figaro.env.locale!
   end
 end
